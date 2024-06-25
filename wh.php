fa3ada5f42944f44b19195d4145696acec7808b8
Umut <develop@kariyerfora.com>
2024-06-25 11:17:45 +0300

    a

--- wh.php
+++ wh.php
@@ -7,33 +7,20 @@ include '_header.php';
 
 ?>
 <div class="container mt-5">
-    <div class="jumbotron m-5 p-5">
-        <center>
-            <h1>IWA Depo Yönetime hoş geldiniz, <span class="username"><?= $_SESSION['user_info']['name'] ?></span></h1>
-            <p>Lütfen yapmak istediğiniz işlemi seçiniz.</p>
-        </center>
+    <div class="jumbotron m-5 p-5 text-center">
+        <h1>IWA Depo Yönetime hoş geldiniz, <span class="username"><?= $_SESSION['user_info']['name'] ?></span></h1>
+        <p>Lütfen yapmak istediğiniz işlemi seçiniz.</p>
     </div>
-    <div class="m-3">
-        <a href="wh_" class="btn btn-primary">Ürün Bul</a>
+    <div class="d-grid gap-2 m-3">
+        <a href="wh_" class="btn btn-primary btn-lg">Ürün Bul</a>
+        <a href="wh_" class="btn btn-primary btn-lg">Kutu Bul</a>
+        <a href="wh_" class="btn btn-primary btn-lg">Raftan/Kutudan Ürün Al</a>
+        <a href="wh_" class="btn btn-primary btn-lg">Rafa/Kutuya Ürün Koy</a>
+        <a href="wh_" class="btn btn-primary btn-lg">Raftan Kutu Al</a>
+        <a href="wh_" class="btn btn-primary btn-lg">Rafa Kutu Koy</a>
+        <a href="wh_" class="btn btn-primary btn-lg">Güncel Depo Listesi</a>
     </div>
-    <div class="m-3">
-        <a href="wh_" class="btn btn-primary">Kutu Bul </a>
-    </div>
-    <div class="m-3">
-        <a href="wh_" class="btn btn-primary">Raftan/Kutudan Ürün Al</a>
-    </div>
-    <div class="m-3">
-        <a href="wh_" class="btn btn-primary">Rafa/Kutuya Ürün Koy</a>
-    </div>
-    <div class="m-3">
-        <a href="wh_" class="btn btn-primary">Raftan Kutu Al</a>
-    </div>
-    <div class="m-3">
-        <a href="wh_" class="btn btn-primary">Rafa Kutu Koy</a>
-    </div>
-    <div class="m-3">
-        <a href="wh_" class="btn btn-primary">Güncel Depo Listesi</a>
-    </div>
+</div>
 </div>
 <?php
 
