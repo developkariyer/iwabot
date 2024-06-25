d40b8ca14b2b0f115f0e1402c71f1a90c2619301
Umut <develop@kariyerfora.com>
2024-06-25 18:03:30 +0300

    a

--- wh_shelf_product_process.php
+++ wh_shelf_product_process.php
@@ -4,6 +4,7 @@ require_once('_login.php');
 require_once('_init.php');
 
 if (!isset($_POST['shelf'])) {
+    error_log('No shelf');
     header('Location: wh_shelf_select.php');
     exit;
 }
@@ -12,6 +13,7 @@ if (!isset($_POST['shelf'])) {
 $stmt = $GLOBALS['pdo']->prepare('SELECT * FROM wh_shelf WHERE id = :id LIMIT 1');
 $stmt->execute(['id' => $_POST['shelf']]);
 if (!$shelf = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    error_log('Shelf not found');
     header('Location: wh_shelf_select.php');
     exit;
 }
@@ -23,6 +25,7 @@ if (
         !isset($_POST['actionType']) ||
         !in_array($_POST['actionType'], ['take', 'put'])
     ) {
+        error_log('Invalid post data');
     header('Location: wh_shelf_product.php?shelf='.urlencode($_POST['shelf']));
     exit;
 }
@@ -31,6 +34,7 @@ if (
 $stmt = $GLOBALS['pdo']->prepare('SELECT * FROM wh_product WHERE fnsku = :fnsku LIMIT 1');
 $stmt->execute(['fnsku' => $_POST['barcode']]);
 if (!$product = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    error_log('Product not found');
     header('Location: wh_shelf_product.php?shelf='.urlencode($_POST['shelf']));
     exit;
 }
@@ -41,15 +45,18 @@ if ($_POST['actionType'] == 'take') {
     $stmt->execute(['product_id' => $product['id'], 'shelf_id' => $shelf['id']]);
     $stock = $stmt->fetchColumn();
     if ($stock < $_POST['stock']) {
+        error_log('Not enough stock');
         header('Location: wh_shelf_product.php?shelf='.urlencode($_POST['shelf']));
         exit;
     }
 }
 
 if ($_POST['actionType'] == 'take') {
+    error_log('Taking stock');
     $stmt = $GLOBALS['pdo']->prepare('DELETE FROM wh_shelf_product WHERE product_id = :product_id AND shelf_id = :shelf_id LIMIT :stock');
     $stmt->execute(['product_id' => $product['id'], 'shelf_id' => $shelf['id'], 'stock' => $_POST['stock']]);
 } else {
+    error_log('Putting stock');
     $stmt = $GLOBALS['pdo']->prepare('INSERT INTO wh_shelf_product (product_id, shelf_id) VALUES (:product_id, :shelf_id)');
     for ($t=0;$t<$_POST['stock'];$t++) {
         $stmt->execute(['product_id' => $product['id'], 'shelf_id' => $shelf['id']]);
