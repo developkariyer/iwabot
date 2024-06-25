5eb1cfb3517df8d3ccf0512c5045b8b4f9e25cb2
Umut <develop@kariyerfora.com>
2024-06-25 14:37:40 +0300

    a

--- wh_shelf_select.php
+++ wh_shelf_select.php
@@ -13,25 +13,24 @@ include '_header.php';
 
 ?>
 <div class="container mt-5">
-    <div class="mt-5">
+    <div class="mt-4">
         <h2>Raf / Koli Seçin</h2>
         <form action="wh_shelf_product.php" method="POST">
             <div class="mb-3">
                 <label for="shelf" class="form-label">Raf / Koli Seçin</label>
                 <select class="form-select" id="shelf" name="shelf" required>
-                    <option value="">Seçin</option>
                     <?php foreach ($shelf as $s): ?>
                         <option value="<?= $s['id'] ?>"><?= $s['name'] ?> (<?= $s['type'] ?><?= $s['parent_id'] ? ' / '.$shelf[$s['parent_id']]['name'] : '' ?>)</option>
                     <?php endforeach; ?>
                 </select>
             </div>
-            <button type="submit" class="btn btn-primary">Devam</button>
-
+            <button type="submit" class="btn btn-primary w-100">Devam</button>
+        </form>
     </div>
-    <div class="d-grid gap-2 m-3 mt-4">
-        <a href="./wh.php" class="btn btn-secondary btn-lg">Depo Yönetim Ana Sayfa</a>
-        <a href="./" class="btn btn-secondary btn-lg">Ana Sayfa</a>
-        <a href="./?logout=1" class="btn btn-danger btn-lg">Logout</a>
+    <div class="d-grid gap-2">
+        <a href="./wh.php" class="btn btn-secondary btn-lg w-100">Depo Yönetim Ana Sayfa</a>
+        <a href="./" class="btn btn-secondary btn-lg w-100">Ana Sayfa</a>
+        <a href="./?logout=1" class="btn btn-danger btn-lg w-100">Logout</a>
     </div>
 </div>
 
