6ea9440cda31fcff5ffa6bfc99bc7732753b90fd
Umut <develop@kariyerfora.com>
2024-06-26 11:15:29 +0300

    modal label

--- wh_shelf_product.php
+++ wh_shelf_product.php
@@ -104,7 +104,7 @@ if ($shelfDetails['parent_id']) {
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="confirmModalLabel">Ürün Bilgileri</h5>
+                <h5 class="modal-title" id="confirmModalLabel"><?= $shelfDetails['name'] ?> / <?= $shelfDetails['type'] ?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form id="barcodeForm" action="wh_shelf_product_process.php" method="POST">
