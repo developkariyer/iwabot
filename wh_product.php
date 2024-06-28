1692080a63187f0efaa74558d5542101090d02ed
Umut <develop@kariyerfora.com>
2024-06-28 17:31:11 +0300

    a

--- wh_product.php
+++ wh_product.php
@@ -28,7 +28,7 @@ include '_header.php';
         <h5>Ürün Bilgileri</h5>
         <p><?= $product->productInfo() ?></p>
         <h5>Ürünün Bulunduğu Yerler</h5>
-        <div class="row g-3 m-3 mt-5">
+        <div class="g-3 m-3 mt-5">
             <?php foreach ($product->getShelves() as $shelf): ?>
                 <a href="wh_product_action.php?product=<?= $product->fnsku ?>&shelf=<?= $shelf->id ?>" class="btn btn-outline-primary rounded-pill w-100 py-3">
                     <?= $shelf->name ?> / <?= $shelf->type ?><?= $shelf->parent ? ' / '.$shelf->parent->name : '' ?>
