867e07426f91ca9dea564fe97d209678272097fc
Umut <develop@kariyerfora.com>
2024-06-28 17:30:36 +0300

    a

--- wh_product.php
+++ wh_product.php
@@ -28,12 +28,14 @@ include '_header.php';
         <h5>Ürün Bilgileri</h5>
         <p><?= $product->productInfo() ?></p>
         <h5>Ürünün Bulunduğu Yerler</h5>
-        <?php foreach ($product->getShelves() as $shelf): ?>
-            <a href="wh_product_action.php?product=<?= $product->fnsku ?>&shelf=<?= $shelf->id ?>" class="btn btn-outline-primary rounded-pill w-100 m-3">
-                <?= $shelf->name ?> / <?= $shelf->type ?><?= $shelf->parent ? ' / '.$shelf->parent->name : '' ?>
-                <br>Raf Mevcudu: <?= $product->shelfCount($shelf) ?> adet
-            </a>
-        <?php endforeach; ?>
+        <div class="row g-3 m-3 mt-5">
+            <?php foreach ($product->getShelves() as $shelf): ?>
+                <a href="wh_product_action.php?product=<?= $product->fnsku ?>&shelf=<?= $shelf->id ?>" class="btn btn-outline-primary rounded-pill w-100 py-3">
+                    <?= $shelf->name ?> / <?= $shelf->type ?><?= $shelf->parent ? ' / '.$shelf->parent->name : '' ?>
+                    <br>Raf Mevcudu: <?= $product->shelfCount($shelf) ?> adet
+                </a>
+            <?php endforeach; ?>
+        </div>
     </div>
     <?= wh_menu() ?>
 </div>
