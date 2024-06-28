935f0922a6b60705914c8ec05753a90f89d1a83c
Umut <develop@kariyerfora.com>
2024-06-28 13:13:41 +0300

    grid

--- wh.php
+++ wh.php
@@ -51,10 +51,18 @@ include '_header.php';
 
     <div class="row g-3 m-3 mt-5">
         <div class="col-md-6">
-            <a href="./" class="btn btn-secondary btn-lg rounded-pill w-100 py-3">Ana Sayfa</a>
+            <a href="wh.php" class="btn btn-secondary btn-lg rounded-pill w-100 py-3">Depo Ana Sayfa</a>
         </div>
         <div class="col-md-6">
-            <a href="./?logout=1" class="btn btn-danger btn-lg rounded-pill w-100 py-3">Logout</a>
+            <a href="./" class="btn btn-secondary btn-lg rounded-pill w-100 py-3">Ana Sayfa</a>
+        </div>
+    </div>
+
+    <div class="row g-3 m-3 mt-5">
+        <div class="col-md-3">
+        </div>
+        <div class="col-md-6">
+            <a href="./?logout=1" class="btn btn-danger btn-lg rounded-pill w-100 py-3">Çıkış</a>
         </div>
     </div>
 </div>
