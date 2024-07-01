62b6f7798303cdb4437bb05ed300a223c575ef29
Umut <develop@kariyerfora.com>
2024-07-01 22:58:02 +0300

    a

--- wh_shelf_search.php
+++ wh_shelf_search.php
@@ -13,18 +13,18 @@ include '_header.php';
 <div class="container mt-5">
     <div class="mt-5">
         <h2>Raflar ve Koliler</h2>
-        <div class="accordion collapse" id="productAccordion">
+        <div class="accordion" id="shelfAccordion">
             <?php foreach ($shelfList as $index => $shelf): ?>
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="heading<?= $index ?>">
-                        <button class="accordion-button btn-success collapsed d-flex justify-content-between align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $index ?>" aria-expanded="false" aria-controls="collapse<?= $index ?>">
-                            <span><strong><?= htmlspecialchars($shelf->name) ?> / <?= htmlspecialchars($shelf->type) ?></strong></span>
+                        <button class="accordion-button collapsed d-flex justify-content-between align-items-center text-start" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $index ?>" aria-expanded="false" aria-controls="collapse<?= $index ?>">
+                            <strong><?= htmlspecialchars($shelf->name) ?> / <?= htmlspecialchars($shelf->type) ?></strong>
                         </button>
                     </h2>
-                    <div id="collapse<?= $index ?>" class="accordion-collapse collapse" aria-labelledby="heading<?= $index ?>" data-bs-parent="#productAccordion">
+                    <div id="collapse<?= $index ?>" class="accordion-collapse collapse" aria-labelledby="heading<?= $index ?>" data-bs-parent="#shelfAccordion">
                         <div class="accordion-body">
-                            Raf içerikleri
-                            <a href="wh_shelf.php?shelf=<?= urlencode($shelf->id) ?>" class="btn btn-outline-success btn-lg rounded-pill w-100 py-3 mt-2">Seç</a>
+                            <p>Raf içerikleri</p>
+                            <a href="wh_shelf.php?shelf=<?= urlencode($shelf->id) ?>" class="btn btn-outline-success btn-lg w-100 py-2 mt-2">Seç</a>
                         </div>
                     </div>
                 </div>
