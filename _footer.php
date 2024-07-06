0f09fab08fb091b725cfce4d6e85945ce57db836
Umut <develop@kariyerfora.com>
2024-07-06 14:47:53 +0300

    a

--- _footer.php
+++ _footer.php
@@ -1,7 +1,8 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap5-toggle@5.0.4/js/bootstrap5-toggle.jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>    <style>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <?= isset($GLOBALS['footer_script']) ? "<script>{$GLOBALS['footer_script']}</script>" : "" ?>
+
 </body>
 </html>
