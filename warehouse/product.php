569c4e37a8f527bcb36c0ac190dcc3dcee687a56
Umut <develop@kariyerfora.com>
2024-07-06 16:32:19 +0300

    a

--- warehouse/product.php
+++ warehouse/product.php
@@ -65,7 +65,9 @@ $unfulfilledProducts = WarehouseProduct::getUnfulfilledProducts();
                 <div class="accordion-body p-0 w-100">
                     <?= productSelect() ?>
                     <div id="selectedProduct">
-                        <p>Ürün Bilgileri</p>
+                        <div class="p3">
+                            <p>Ürün Bilgileri</p>
+                        </div>
                         <!-- First Accordion -->
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="headingFirst">
