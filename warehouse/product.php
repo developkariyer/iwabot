529f8f8e6ec781b2cbc46c79db7547dfb158d181
Umut <develop@kariyerfora.com>
2024-07-06 11:22:22 +0300

    a

--- warehouse/product.php
+++ warehouse/product.php
@@ -4,6 +4,7 @@ require_once('warehouse.php');
 
 include '../_header.php';
 
+$unfulfilledProducts = WarehouseProduct::getUnfulfilledProducts();
 ?>
 
 <div class="container mt-5">
@@ -12,11 +13,11 @@ include '../_header.php';
         <p>İşlem yapmak istediğiniz ürünü seçiniz. Depo Ana Menü için <a href="./">buraya basınız.</a></p>
     </div>
 
-    <h4 data-bs-toggle="collapse" data-bs-target="#productAccordion" aria-expanded="true" aria-controls="productAccordion">
-        Çıkış İçin Bekleyen Ürünler <small>(<?= count(WarehouseProduct::getUnfulfilledProducts()) ?> adet) <i>Görmek için basınız</i></small>
-    </h4>
+    <button class="btn btn-primary mb-3" data-bs-toggle="collapse" data-bs-target="#productAccordion" aria-expanded="true" aria-controls="productAccordion">
+        Çıkış İçin Bekleyen Ürünler <small>(<?= count($unfulfilledProducts) ?> adet) <i>Görmek için basınız</i></small>
+    </button>
     <div class="accordion collapse" id="productAccordion">
-        <?php foreach (WarehouseProduct::getUnfulfilledProducts() as $index => $product): ?>
+        <?php foreach ($unfulfilledProducts as $index => $product): ?>
             <div class="accordion-item">
                 <h2 class="accordion-header" id="heading<?= $index ?>">
                     <button class="accordion-button btn-success collapsed d-flex justify-content-between align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $index ?>" aria-expanded="false" aria-controls="collapse<?= $index ?>">
@@ -32,6 +33,13 @@ include '../_header.php';
                 </div>
             </div>
         <?php endforeach; ?>
+        <?php if (empty($unfulfilledProducts) || count($unfulfilledProducts) == 0): ?>
+            <div class="accordion-item">
+                <div class="accordion-body">
+                    <p>Çıkış için bekleyen ürün bulunmamaktadır.</p>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
 
     <?= wh_menu() ?>
