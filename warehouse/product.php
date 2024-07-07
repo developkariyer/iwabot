55cc689bdb1f03965a39b6c541588d412b86d1ea
Umut <develop@kariyerfora.com>
2024-07-07 14:06:54 +0300

    a

--- warehouse/product.php
+++ warehouse/product.php
@@ -116,7 +116,7 @@ if (isset($_GET['product_id']) && !empty($_GET['product_id']) && is_numeric($_GE
                 </button>
             </h2>
             <div id="productAccordion3" class="accordion-collapse collapse <?= $product_id ? 'show' : '' ?>" aria-labelledby="headingMain3" data-bs-parent="#mainAccordion">
-                <div class="accordion-body p-0 w-100">
+                <div class="accordion-body p-5 w-100">
                     <form action="controller.php" method="POST">
                         <input type="hidden" name="action" value="add_product">
                         <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?>">
