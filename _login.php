0e4cbd93cebd507513c9543a02821325f615ae0c
Umut <develop@kariyerfora.com>
2024-07-08 09:46:10 +0300

    a

--- _login.php
+++ _login.php
@@ -3,11 +3,12 @@ ini_set('session.gc_maxlifetime', 86400);
 ini_set('session.cookie_lifetime', 86400);
 session_start();
 
+$redirectUri = 'https://iwarden.iwaconcept.com/iwabot/login.php';
+$loggedInUri = 'https://iwarden.iwaconcept.com/iwabot/';
+
 if (!isset($guestFree) && !isset($_SESSION['logged_in'])) {
     $_SESSION['prev_url'] = $_SERVER['REQUEST_URI'];
-    header('Location: login.php');
+    header("Location: $redirectUri");
     exit;
 }
 
-$redirectUri = 'https://iwarden.iwaconcept.com/iwabot/login.php';
-$loggedInUri = 'https://iwarden.iwaconcept.com/iwabot/';
