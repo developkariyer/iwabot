4cd8d5b055ede6f967993855b08eea6ad2f311ab
Umut <develop@kariyerfora.com>
2024-07-08 20:50:42 +0300

    a

--- warehouse/inventory.php
+++ warehouse/inventory.php
@@ -77,7 +77,7 @@ $products = WarehouseProduct::getAll();
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="headingProduct<?= $index ?>">
                                 <button class="accordion-button collapsed d-flex justify-content-between align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#collapseProduct<?= $index ?>" aria-expanded="false" aria-controls="collapseProduct<?= $index ?>">
-                                    <span><strong><?= htmlspecialchars($product->name) ?> (<?= htmlspecialchars($product->code) ?>)</strong></span>
+                                    <span><strong><?= htmlspecialchars($product->name) ?> (<?= htmlspecialchars($product->fnsku) ?>)</strong></span>
                                 </button>
                             </h2>
                             <div id="collapseProduct<?= $index ?>" class="accordion-collapse collapse" aria-labelledby="headingProduct<?= $index ?>" data-bs-parent="#inventoryAccordion2">
