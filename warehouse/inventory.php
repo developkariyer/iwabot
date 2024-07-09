1b59812323b536319725035b113b94e557c8b878
Umut <develop@kariyerfora.com>
2024-07-09 23:09:55 +0300

    a

--- warehouse/inventory.php
+++ warehouse/inventory.php
@@ -89,7 +89,6 @@ $products = WarehouseProduct::getAll();
                                     <?= containerInfo($raf) ?><br>
                                     <strong>Raftaki Koliler:</strong><br>
                                     <?php foreach ($raf->getChildren() as $childIndex => $child): ?>
-                                        <?php if ($child->getTotalCount() == 0) continue; ?>
                                         <div class="accordion-item">
                                             <h2 class="accordion-header box-h2" id="headingChild<?= $index ?>-<?= $childIndex ?>">
                                                 <button class="accordion-button collapsed d-flex justify-content-between align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#collapseChild<?= $index ?>-<?= $childIndex ?>" aria-expanded="false" aria-controls="collapseChild<?= $index ?>-<?= $childIndex ?>">
