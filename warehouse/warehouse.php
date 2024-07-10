d4f30d909ae127ad3ca9ed5d70784020e5f7a9d8
Umut <develop@kariyerfora.com>
2024-07-11 01:20:42 +0300

    a

--- warehouse/warehouse.php
+++ warehouse/warehouse.php
@@ -142,7 +142,7 @@ function productInfo($product) {
     $fnsku = htmlspecialchars($product->fnsku, ENT_QUOTES, 'UTF-8');
     return "
         <b>Ürün Adı:</b> {$product->name}<br>
-        <b>FNSKU:</b> <span id='fnsku'>{$fnsku}</span> <span class='badge badge-primary' style='cursor: pointer;' onclick='copyToClipboard(\"fnsku\")'>Kopyala</span><br>
+        <b>FNSKU:</b> <span id='fnsku'>{$fnsku}</span> <span class='badge bg-primary' style='cursor: pointer;' onclick='copyToClipboard(\"fnsku\")'>Kopyala</span><br>
         <b>Kategori:</b> {$product->category}<br>
         <b>IWASKU:</b> {$product->iwasku}<br>
         <b>Özellikler (metrik):</b><br>{$product->dimension1}x{$product->dimension2}x{$product->dimension3}cm, {$product->weight}kg<br>
