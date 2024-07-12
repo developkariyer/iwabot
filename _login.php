3b927aa8619b49014f92be067a8c8b0790044d70
Umut <develop@kariyerfora.com>
2024-07-12 10:48:06 +0300

    a

--- _login.php
+++ _login.php
@@ -6,9 +6,16 @@ session_start();
 $redirectUri = 'https://iwarden.iwaconcept.com/iwabot/login.php';
 $loggedInUri = 'https://iwarden.iwaconcept.com/iwabot/';
 
+$redirectUri1 = 'https://depo.iwa.web.tr/login.php';
+$loggedInUri1 = 'https://depo.iwa.web.tr/';
+
+if (strpos($_SERVER['REQUEST_URI'], 'depo.iwa.web.tr') !== false) {
+    $redirectUri = $redirectUri1;
+    $loggedInUri = $loggedInUri1;
+}
+
 if (!isset($guestFree) && !isset($_SESSION['logged_in'])) {
     $_SESSION['prev_url'] = $_SERVER['REQUEST_URI'];
     header("Location: $redirectUri");
     exit;
 }
-
