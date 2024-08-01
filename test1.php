6861114cc612d96eb216ab1592485fb2c4ced3d8
Umut <develop@kariyerfora.com>
2024-08-01 22:32:42 +0300

    a

--- test1.php
+++ test1.php
@@ -70,11 +70,11 @@ $pdf->SetMargins(0, 0, 0);
 // Set font for the big text
 $pdf->SetFont('Arial', 'B', 20);
 $pdf->SetXY(0, 0);
-$pdf->Cell(0, 5, '31-1234', 0, 0, 'C');
+$pdf->Cell(0, 6, '31-1234', 0, 0, 'C');
 
 // Set font for the small text
 $pdf->SetFont('Arial', '', 4);
-$pdf->SetXY(0, 10);
+$pdf->SetXY(0, 7);
 $text = "MAŞALLAH TEBAREKALLAH GOLD 69 CM (B0CD1WN9BZ) x 3\nMAŞALLAH TEBAREKALLAH GOLD 69 CM (B0CD1WN9BZ) x 3\nMAŞALLAH TEBAREKALLAH GOLD 69 CM (B0CD1WN9BZ) x 3\nMAŞALLAH TEBAREKALLAH GOLD 69 CM (B0CD1WN9BZ) x 3\nMAŞALLAH TEBAREKALLAH GOLD 69 CM (B0CD1WN9BZ) x 3";
 $pdf->MultiCell(40, 1.2, $text, 0, 'L');
 
