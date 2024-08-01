9949139a3298682eaf0c08d2fe2093f4c3adc816
Umut <develop@kariyerfora.com>
2024-08-01 21:45:36 +0300

    a

--- test1.php
+++ test1.php
@@ -85,7 +85,7 @@ $pageHeight = 40;
 $pdf->AddPage('L', [$pageWidth, $pageHeight]);
 
 // Add the QR code image
-$pdf->Image('qrcode.png', 5, 5, 25, 25); // 2.5cm x 2.5cm
+$pdf->Image('qrcode.png', 2, 2, 25, 25); // 2.5cm x 2.5cm
 
 // Set font for the big text
 $pdf->SetFont('Arial', 'B', 14);
