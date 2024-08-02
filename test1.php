eb9795fd165c8f2534a2963361cab8ab7aa4640f
Umut <develop@kariyerfora.com>
2024-08-02 11:59:53 +0300

    a

--- test1.php
+++ test1.php
@@ -72,7 +72,7 @@ $pdf->SetFont('Arial', 'B', 24);
 $pdf->SetXY(0, 2);
 $pdf->Cell(0, 7, '31-1234', 0, 0, 'C');
 $pdf->SetFont('Arial', '', 12);
-$pdf->SetXY(10, 13);
+$pdf->SetXY(12, 11);
 $text = "iwa Concept\n";
 $text .= date('Y-m');
 $pdf->MultiCell(25, 5, removeTRChars($text), 0, 'L');
