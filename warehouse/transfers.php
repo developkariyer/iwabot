8d117ec7a99c2cea6f996ed348d593b5260f60f3
Umut <develop@kariyerfora.com>
2024-08-18 10:18:09 +0300

    a

--- warehouse/transfers.php
+++ warehouse/transfers.php
@@ -54,7 +54,7 @@ include '../_header.php';
                                         <td><strong><?= $order->item_type === 'WarehouseProduct' ? 'Ürün' : 'Koli' ?></strong><br><?= htmlspecialchars($order->object->name) ?><br>(<?= $order->object instanceof WarehouseProduct ? htmlspecialchars($order->object->fnsku) : htmlspecialchars($order->object->parent->name) ?>)</td>
                                         <td><?= nl2br(htmlspecialchars($order->description)) ?></td>
                                         <td><strong><?= $logAdd ? $logAdd->username() : '' ?></strong><br><?= htmlspecialchars($order->created_at) ?></td>
-                                        <td><strong><?= $logFulfil ? $logFulfil->username() : '' ?></strong><br><?= htmlspecialchars($order->fulfilled_at) ?></td>
+                                        <td class="<?= !$order->fulfilled_at ? 'bg-danger text-white' : '' ?>"><strong><?= $logFulfil ? $logFulfil->username() : '' ?></strong><br><?= htmlspecialchars($order->fulfilled_at) ?></td>
                                     </tr>
                                 <?php endforeach; ?>
                             </tbody>
