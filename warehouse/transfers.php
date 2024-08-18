bffe65fcac41510a7653d1ad0c4dc2d2e5f0e67f
Umut <develop@kariyerfora.com>
2024-08-18 10:22:12 +0300

    a

--- warehouse/transfers.php
+++ warehouse/transfers.php
@@ -51,10 +51,20 @@ include '../_header.php';
                                     ?>
                                     <tr>
                                         <td class="<?= !$order->fulfilled_at ? 'bg-warning' : '' ?>">#<?= $order->id ?></td>
-                                        <td class="<?= !$order->fulfilled_at ? 'bg-warning' : '' ?>"></td><strong><?= $order->item_type === 'WarehouseProduct' ? 'Ürün' : 'Koli' ?></strong><br><?= htmlspecialchars($order->object->name) ?><br>(<?= $order->object instanceof WarehouseProduct ? htmlspecialchars($order->object->fnsku) : htmlspecialchars($order->object->parent->name) ?>)</td>
+                                        <td class="<?= !$order->fulfilled_at ? 'bg-warning' : '' ?>">
+                                            <strong><?= $order->item_type === 'WarehouseProduct' ? 'Ürün' : 'Koli' ?></strong><br>
+                                            <?= htmlspecialchars($order->object->name) ?><br>
+                                            (<?= $order->object instanceof WarehouseProduct ? htmlspecialchars($order->object->fnsku) : htmlspecialchars($order->object->parent->name) ?>)
+                                        </td>
                                         <td class="<?= !$order->fulfilled_at ? 'bg-warning' : '' ?>"><?= nl2br(htmlspecialchars($order->description)) ?></td>
-                                        <td class="<?= !$order->fulfilled_at ? 'bg-warning' : '' ?>"></td><strong><?= $logAdd ? $logAdd->username() : '' ?></strong><br><?= htmlspecialchars($order->created_at) ?></td>
-                                        <td class="<?= !$order->fulfilled_at ? 'bg-warning' : '' ?>"><strong><?= $logFulfil ? $logFulfil->username() : '' ?></strong><br><?= htmlspecialchars($order->fulfilled_at) ?></td>
+                                        <td class="<?= !$order->fulfilled_at ? 'bg-warning' : '' ?>">
+                                            <strong><?= $logAdd ? $logAdd->username() : '' ?></strong><br>
+                                            <?= htmlspecialchars($order->created_at) ?>
+                                        </td>
+                                        <td class="<?= !$order->fulfilled_at ? 'bg-warning' : '' ?>">
+                                            <strong><?= $logFulfil ? $logFulfil->username() : '' ?></strong><br>
+                                            <?= htmlspecialchars($order->fulfilled_at) ?>
+                                        </td>
                                     </tr>
                                 <?php endforeach; ?>
                             </tbody>
